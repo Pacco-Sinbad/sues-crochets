a67bb4ddeee1f30e6484670464f1a22aedafde2a
Mathias Fischer <mfischerf92@gmail.com>
2023-04-11 16:47:50 -0500

    this is a basic layout for the landing page for sues site. I will have to check with her to see if this is what she had in mind. addtionally i need to see if i can use multiple pages or not. I think this site would benefit from using multiple pages especially as the viewer window size decreases which is likely that viewers would look at the page with multiple screens between browsing and purchasing.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
+document.querySelector('.burger').addEventListener('click', nav);
+
+function nav(){
+    document.querySelector('nav').style.visibility = 'visible'   
+};
+
+document.querySelector('.closeMenu').addEventListener('click', navHide);
+
+function navHide(){
+    document.querySelector('nav').style.visibility = 'collapse'   
+};
+
+
+
 
